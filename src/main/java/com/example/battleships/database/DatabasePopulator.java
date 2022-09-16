0fdf7286617b29f5ddfbc27f689b18799c893cf6
MarkusKeck <markus@keck.bayern>
2022-09-16 09:25:40 +0200

    Added different databases for test and prod

--- src/main/java/com/example/battleships/database/DatabasePopulator.java
+++ src/main/java/com/example/battleships/database/DatabasePopulator.java
@@ -1,6 +1,7 @@
 package com.example.battleships.database;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -8,8 +9,14 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public final class DatabasePopulator implements CommandLineRunner {
 
+    @Value("${battleship.eigene-property}")
+    private String meineEigeneProperty;
+
     @Override
     public void run(String... args) {
+
+        System.out.println(meineEigeneProperty);
+
         // The DatabasePopulator seems not to be that popular
     }
 
